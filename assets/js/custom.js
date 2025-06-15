4847a4315a312898e210052b552906567df98aaf
Haqdad Khan <haqdadkhan.dev@gmail.com>
2025-06-15 18:07:55 +0500

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".all-games-carousel").owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoWidth: true,
+        responsive: {
+            0: { items: 2 },
+            480: { items: 3 },
+            768: { items: 4 },
+            992: { items: 5 },
+            1200: { items: 6 }
+        }
+    });
+});
