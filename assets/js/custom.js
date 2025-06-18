86507736897e6c70864ace80caeef4e2e9df71c7
Haqdad Khan <haqdadkhan.dev@gmail.com>
2025-06-18 18:08:47 +0500

    sidebar updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,19 @@
 // --- ALL GAMES CAROUSEL ---
 $(document).ready(function (e) {
-    $(".all-games-carousel").owlCarousel({
+    $(".all-nav-carousel").owlCarousel({
         loop: false,
         margin: 10,
         nav: false,
         dots: false,
         autoWidth: true,
-        responsive: {
-            0: { items: 2 },
-            480: { items: 3 },
-            768: { items: 4 },
-            992: { items: 5 },
-            1200: { items: 6 }
-        }
+        // responsive: {
+        //     0: { items: 2 },
+        //     480: { items: 3 },
+        //     768: { items: 4 },
+        //     992: { items: 5 },
+        //     1200: { items: 6 }
+        // }
     });
-    $('.all-games-carousel').on('click', '.nav-link', function (e) {
-        e.stopPropagation();
-    });
-
-    e.stopPropagation();
 });
 
 // --- BOTTOM NAV JS ---
@@ -28,3 +23,24 @@ function setActive(clickedItem) {
     });
     clickedItem.classList.add('active');
 }
+
+// SIDEBAR ACTIVE/NOT-ACTIVE
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".main-menu a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        if (window.scrollY >= section.offsetTop - 100 && window.scrollY < section.offsetTop + section.offsetHeight - 100) {
+            current = section.id;
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.parentElement.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.parentElement.classList.add("active");
+        }
+    });
+});
